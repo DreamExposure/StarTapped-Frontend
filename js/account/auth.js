b6887100b25e7d2ca32b00b174d485b1c887146c
NovaFox161 <novamaday@gmail.com>
2018-12-11 20:32:20 -0600

    Add remember me option on sign in.

--- js/account/auth.js
+++ js/account/auth.js
@@ -1,18 +1,29 @@
 function saveCredentials(json) {
 	//Set defaults...
-	cookie.defaults.expires = 7;
 	cookie.defaults.secure = true;
 	cookie.defaults.domain = "www.startapped.com";
 
 
-	cookie.set({
-		"auth": "auth",
-		"access": json.access_token,
-		"refresh": json.refresh_token,
-		"expire": json.expire
-	}, {
-		secure: true
-	});
+	if (document.getElementById("remember-me-sign-in").checked) {
+		cookie.set({
+			"auth": "auth",
+			"access": json.access_token,
+			"refresh": json.refresh_token,
+			"expire": json.expire
+		}, {
+			secure: true,
+			expires: 7
+		});
+	} else {
+		cookie.set({
+			"auth": "auth",
+			"access": json.access_token,
+			"refresh": json.refresh_token,
+			"expire": json.expire
+		}, {
+			secure: true
+		});
+	}
 }
 
 function getCredentials() {
